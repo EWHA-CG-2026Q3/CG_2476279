ee1045ec7d697d7c131f4fe60c0811775e5c277e
jjihye-0108 <wjswlgp9511@gmail.com>
2026-09-15 14:20:37 +0900

    S03 assignment first triangle

--- Assets/S03/S03_Mesh2D/S03_CustomPolygonMesh_Square.cs
+++ Assets/S03/S03_Mesh2D/S03_CustomPolygonMesh_Square.cs
@@ -8,18 +8,15 @@
           // TODO 1: 원하는 다각형의 정점 좌표를 채우세요 (최소 4개)
           Vector3[] vertices = new Vector3[]
           {
-              new Vector3(0f, 1f, 0f), //0
-              new Vector3(1f, 1f, 0f), //1
-              new Vector3(1f, 0f, 0f), //2
-              new Vector3(0f, 0f, 0f), //3
-              new Vector3(0f, 0f, 1f), //4
+              new Vector3(-1f, -0.5f, 0f), //0
+              new Vector3(1f, -0.5f, 0f), //1
+              new Vector3(1f, 0.5f, 0f), //2
           };
 
           // TODO 2: 정점 3개씩 묶어 삼각형들을 구성하세요
           int[] triangles = new int[]
           {
               0, 1, 2,
-              0, 2, 3,
           };
 
           Mesh mesh = new Mesh();
